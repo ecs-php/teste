4a9a871897b1a92317ab22dfa6b4171125ab5df0
Ercy Moreira Neto <fireball.vb@gmail.com>
2017-02-23 01:00:42 -0300

    teste feito

---
+++ teste/Model/Person.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace TESTE\Model;
+
+use TESTE\Entity\Person as PersonEntity;
+
+/**
+ * Description of Pessoa
+ *
+ * @author pandacoder
+ */
+class Person {
+    
+    protected $app;
+            
+    function __construct($app) {
+        $this->app = $app;
+    }
+    
+    public function getAll()
+    {
+        $repository = $this->app['orm.em']->getRepository('TESTE\Entity\Person');
+        $list = $repository->findAll();
+        
+        $response = [];
+        foreach ($list as $person){
+            $response[] =  $person->toArray();
+        }
+        
+        return $response;
+    }
+    
+    public function insert(PersonEntity $person)
+    {
+        return $this->save($person);
+    }
+    
+    public function update(PersonEntity $person)
+    {
+        return $this->save($person);
+    }
+    
+    protected function save(PersonEntity $person)
+    {
+        $this->app['orm.em']->persist($person);
+        $this->app['orm.em']->flush();
+        
+        return $person;
+    }
+
+    public function delete(PersonEntity $person)
+    {
+        if ( $person == NULL){
+            throw new Exception("No data found!");
+        }
+        $this->app['orm.em']->remove($person);
+        $this->app['orm.em']->flush();
+        
+        return $person;
+    }
+}
