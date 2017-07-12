5b97cdcb9059cc30ddcfdffce358ed8a8f96158e
Antônio Marco da Silva <suporte3@toolsystems.com.br>
2017-07-12 17:39:10 -0300

    Controllers done, frontend left

--- src/controllers.php
+++ src/controllers.php
@@ -5,48 +5,72 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use App\Model\Person;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
 
 $app->before(function(Request $request, $app) use ($app){
-    if (strpos($request->headers->get('Content-Type'), 'application/json')!==0) {
-        return $app->json(array('error' => 'Invalid Header Content-Type'), 403);
+    if (0 == strpos($request->headers->get('Content-Type'), 'application/json')) {
+		$data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+	}else{
+        return $app->json(array('message' => 'Invalid Header Content-Type'), 403);
     }
 });
 
-$app->get('/person', function () use ($app) {
-    $users = $app['db']->fetchAll('SELECT * FROM users');
-    return $app->json($users);
+$app->mount('/api', function ($app) use ($app){
+
+	$app->get('/person', function () use ($app) {
+		$person = Person::all();
+		return $app->json($person, 201);
+	});
+	
+	$app->get('/person/{id}', function ($id) use ($app) {
+		$person = Person::find($id);
+		return $app->json($person, 201);
+	});
+
+	$app->put('/person', function (Request $request) use ($app) {
+		try{
+			$person = new Person($request->request->all());
+			$person ->save();
+		} catch (Exception $e){
+			return $app->json(array('message' => 'Invalid data'), 401);
+		}
+		return $app->json($person ,201);
+	});
+
+	$app->put('/person/{id}', function (Request $request, $id) use ($app) {
+		$person = Person::find($id);
+		if($person != null){
+			try{
+				$person->update($request->request->all());
+			}catch (Exception $e){
+				return $app->json(array('message' => 'Invalid data'), 401);
+			}
+		}else{
+			return $app->json(array('message' => 'Person not found'), 401);
+		}
+		return $app->json($person, 201);
+	});
+
+	$app->delete('/person/{id}', function ($id) use ($app) {
+		$person = Person::find($id);
+		if($person != null){
+			try{
+				$person->delete());
+			}catch (Exception $e){
+				return $app->json(array('message' => 'Invalid data'), 401);
+			}
+		}else{
+			return $app->json(array('message' => 'Person not found'), 401);
+		}
+		return $app->json($person, 201);
+	});
 });
-$app->get('/person/{id}', function ($id) use ($app) {
-    $users = $app['db']->fetchAll('SELECT * FROM users WHERE id = '.$id);
-    return $app->json($users);
-});
 
-$app->put('/person', function (Request $request) use ($app) {
-    $dados = json_decode($request->getContent(), true);
-
-    $app['db']->insert('users', array(
-        'name' => $dados['name'],
-        'email' => $dados['email'],
-        'date_birth' => $dados['date_birth'],
-        'address' => $dados['address'],
-        'created_at' => date("Y-m-d H:i:s"),
-        'updated_at' => date("Y-m-d H:i:s"),
-    ));
-    $user = $app['db']->fetchAll('SELECT * FROM users WHERE id = '.$app['db']->lastInsertId());
-    return $app->json($user);
-});
-
-$app->put('/person/{id}', function (Request $request, $id) use ($app) {
-    $dados = json_decode($request->getContent(), true);
-    $app['db']->update('users', $dados, array('id' => $id));
-
-    return $app->json($app['db']->fetchAll('SELECT * FROM users WHERE id = '.$id));
-});
-
-$app->delete('/person/{id}', function ($id) use ($app) {
-    $user = $app['db']->fetchAll('SELECT * FROM users WHERE id = '.$id);
-    $app['db']->delete('users', array('id' => $id));
-    return $app->json($user);
+$app->error(function (Exception $e, $code) use ($app) {
+    if ($app['debug'])
+        return;
+    return $app->json(array('message' => 'An error ocurred'), $code);
 });
