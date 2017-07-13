801ed2a797ad0a2ad95ad86d49231ce4fb0b1274
Antomarsi <antomarsi@hotmail.com>
2017-07-12 21:31:23 -0300

    Removed the frontend, ready to send

--- templates/js/app.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$.get('json/winners.json', function(result){
-		let items = [];
-		$.each(result, function(key, val){
-			items.push('<div class="col-xs-12 col-sm-6"><div class="inline"><div class="calendar-orange sprite">'+val.date+'</div><div class="block-inline"><span class="winner-city">'+val.city+' - '+val.state+'</span><span class="winner-number">Número sorteado: <b>'+val.number+'</b></span></div></div></div>')
-		});
-		$("#winners").append(items);
-	});
-});
