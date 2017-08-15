2e08b8d9f93ff6ea759e7e174fc64d9cd1fc6d38
Mateus Guerra <mateus.guerra@a2c.com.br>
2017-08-15 16:56:24 -0300

    Testing architecture, first routes

--- index.php
+++ index.php
@@ -5,12 +5,37 @@ require_once __DIR__.'/bootstrap.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-$app->after(function (Request $request, Response $response) {
-    $response->headers->set('Access-Control-Allow-Origin', '*');
+$app->before(function (Request $request) {
+    // if (false === strpos($request->headers->get('Content-Type'), 'application/json')) {
+    //     throw new Exception("Request not in json", 400);
+    // }
+
+    $data = json_decode($request->getContent(), true);
+    $request->request->replace(is_array($data) ? $data : array());
+});
+
+$app->error(function (\Exception $e, Request $request, $code) use ($app) {
+    return $app->json($e->getCode() . ' - ' . $e->getMessage());
 });
 
 $app->get('/', function () use ($app){
     return $app->json('', 200);
+});
+
+$app->get('/{id}', function ($id) use ($app){
+    return $app->json($id, 200);
+});
+
+$app->put('/', function () use ($app){
+    return $app->json('NEW', 200);
+});
+
+$app->post('/', function () use ($app){
+    return $app->json('UPDATE', 200);
+});
+
+$app->delete('/', function () use ($app){
+    return $app->json('DELETE', 200);
 });
 
 $app->run();
---
+++ src/Serasa/Model/Entity.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Serasa\Model;
+
+abstract class Entity {
+    
+}
