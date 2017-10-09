d03267d8283b369ed0b25a84ed1fede4da1b55e0
Rogerio Lamarques <rogerio.lamarques@gmail.com>
2017-10-08 22:57:39 -0300

    adaptação de configurações

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'driver'        => 'pdo_mysql',
+    'host'      => 'localhost',
+    'dbname'    => 'linktalentos',
+    'user'      => 'root',
+    'password'  => 'root',
+    'port'      => '3306',
+    'charset'   => 'utf8mb4',
+);
