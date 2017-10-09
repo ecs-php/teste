08126a95f70908742530ef4a8c2cdf74293f7724
Rogerio Lamarques <rogerio.lamarques@gmail.com>
2017-10-09 00:51:10 -0300

    Início do layout prova html

--- web/index.php
+++ web/index.php
@@ -1,10 +1,9 @@
 <?php
-include __DIR__ . '/index_dev.php';
-//ini_set('display_errors', 0);
-//
-//require_once __DIR__.'/../vendor/autoload.php';
-//
-//$app = require __DIR__.'/../src/app.php';
-//require __DIR__.'/../config/prod.php';
-//
-//$app->run();
+ini_set('display_errors', 0);
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = require __DIR__.'/../src/app.php';
+require __DIR__.'/../config/prod.php';
+
+$app->run();
